d989ed3b845ca390ee91b947e562e0f0547f0bd7
curet <jcscuret@gmail.com>
2022-02-21 23:21:16 -0400

    initialization of movieAdapter

--- app/src/main/java/com/example/flixster/MainActivity.kt
+++ app/src/main/java/com/example/flixster/MainActivity.kt
@@ -3,10 +3,13 @@ package com.example.flixster
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.codepath.asynchttpclient.AsyncHttpClient
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import okhttp3.Headers
 import org.json.JSONException
+
 
 private const val TAG = "MainActivity"
 private const val NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"
@@ -15,10 +18,17 @@ class MainActivity : AppCompatActivity() {
 
 
     private val movies = mutableListOf<Movie>()
+    private lateinit var rvMovies: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        rvMovies = findViewById(R.id.rvMovies)
+
+        val movieAdapter = MovieAdapter(this, movies)
+        rvMovies.adapter = movieAdapter
+        rvMovies.layoutManager = LinearLayoutManager(this)
+
 
         val client = AsyncHttpClient()
         client.get(NOW_PLAYING_URL, object: JsonHttpResponseHandler(){
@@ -36,6 +46,7 @@ class MainActivity : AppCompatActivity() {
                 try {
                     val movieJsonArray = json.jsonObject.getJSONArray("results")
                     movies.addAll(Movie.fromJsonArray(movieJsonArray)) // this line might through a JSON exception and is the reason this is going to be into a try..catch
+                    movieAdapter.notifyDataSetChanged()
                     Log.i(TAG, "Movie list $movies")
                 }catch (e: JSONException){
                     Log.e(TAG, "Encountered exception $e")
